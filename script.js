918122c5aa583aa0a337a12a723ff53a8edfb966
tazbir01 <tajberahamad@gmail.com>
2023-09-01 14:17:27 +0600

    added card

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const handleCategory = async () => {
     const categoryContainer = document.getElementById('category-container')
 
     data.data.forEach(element => {
-        // console.log(element.category)
+        // console.log(element.category_id)
         const div = document.createElement('div');
         div.innerHTML = `
             <button onclick="handleCard('${element.category_id}')" class="bg-slate-200 p-2 rounded font-semibold hover:bg-red-500 hover:text-white">${element.category}</button>
@@ -19,23 +19,31 @@ const handleCard = async (categoryId) => {
     // console.log(categoryId)
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const data = await res.json()
-    console.log(data)
+    // console.log(data)
 
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML = ""
     data.data.forEach((element) => {
-        console.log(element.thumbnail)
+        // console.log(element.authors[0].profile_name)
         const div = document.createElement('div')
         div.innerHTML = `
-                <div class="card bg-base-100 shadow-xl">
-                <figure><img src="${element.thumbnail}" alt="" /></figure>
-                    <div class="card-body">
-                    <h2 class="card-title">${element.title}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
-                </div>
+                <div class="card rounded-lg bg-base-100 ">
+                    <figure><img class="rounded-lg h-44" src="${element.thumbnail}" alt="" /></figure>
+                    <div class="my-5">
+                        <div class="flex gap-2">
+                            <div>
+                                <img class="w-10 h-10 rounded-full" src="${element.authors[0].profile_picture}">
+                            </div>
+                            <div>
+                                <h2 class="card-title font-bold">${element.title}</h2>
+                                <p class="font-medium text-slate-600">${element.authors[0].profile_name} 
+                                    <span>${element.authors[0].verified? "<img class='inline' src='./icons/verified-icon.svg'>" :""}</span>
+                                </p>
+                                
+                                <p class="font-medium text-slate-600">${element.others.views} views</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
         `
         cardContainer.appendChild(div)
@@ -43,3 +51,4 @@ const handleCard = async (categoryId) => {
 }
 
 handleCategory()
+handleCard(1000)
