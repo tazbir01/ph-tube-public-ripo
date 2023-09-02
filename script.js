1c82683bf32d84e62c4b8c1276bba045bce743e7
tazbir01 <tajberahamad@gmail.com>
2023-09-02 09:22:31 +0600

    add time on the image

--- script.js
+++ script.js
@@ -19,51 +19,61 @@ const handleCard = async (categoryId) => {
     // console.log(categoryId)
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const data = await res.json()
-    console.log(data.data.length)
+    // console.log(data.data[0].others.views.sort())
     
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML = ""
 
+    const emptyContainer = document.getElementById('empty-container')
+    emptyContainer.innerHTML = ""
+
+
     if(data.data.length === 0){
-        const emptyContainer = document.getElementById('empty-container')
         const div = document.createElement('div')
         
         div.innerHTML = `
-            <div class=" flex justify-center">
+            <div class="flex justify-center">
                 <div>
-                    <img src="./icons/icon.png">
-                    <p>Oops!! Sorry, There is no content here</p>
+                    <div class="flex justify-center">
+                        <img src="./icons/icon.png">
+                    </div>
+                    <p class="text-2xl font-bold">Oops!! Sorry, There is no content here</p>
                 </div>
             </div>
         `
         emptyContainer.appendChild(div)
     }
-
-    data.data.forEach((element) => {
-        // console.log(element.authors[0].profile_name)
-        const div = document.createElement('div')
-        div.innerHTML = `
-                <div class="card rounded-lg bg-base-100 ">
-                    <figure><img class="rounded-lg w-72 h-44" src="${element.thumbnail}" alt="" /></figure>
-                    <div class="my-5 mx-2">
-                        <div class="flex gap-2">
-                            <div>
-                                <img class="w-10 h-10 rounded-full" src="${element.authors[0].profile_picture}">
-                            </div>
-                            <div>
-                                <h2 class="card-title font-bold">${element.title}</h2>
-                                <p class="font-medium text-slate-600">${element.authors[0].profile_name} 
-                                    <span>${element.authors[0].verified? "<img class='inline' src='./icons/verified-icon.svg'>" :""}</span>
-                                </p>
-                                
-                                <p class="font-medium text-slate-600">${element.others.views} views</p>
+    else{
+        data.data.forEach((element) => {
+            // console.log(element.authors[0].profile_name)
+            const div = document.createElement('div')
+            div.innerHTML = `
+                    <div class="card rounded-lg bg-base-100 ">
+                        <figure class="relative">
+                            <img class="rounded-lg w-72 h-44 z-0" src="${element.thumbnail}" alt="" />
+                            <p class="bg-zinc-900 text-white rounded absolute right-6 bottom-3 z-10 px-2">${element.others.posted_date}</p>
+                        </figure>
+                        <div class="my-5 mx-2">
+                            <div class="flex gap-2">
+                                <div>
+                                    <img class="w-10 h-10 rounded-full" src="${element.authors[0].profile_picture}">
+                                </div>
+                                <div>
+                                    <h2 class="card-title font-bold">${element.title}</h2>
+                                    <p class="font-medium text-slate-600">${element.authors[0].profile_name} 
+                                        <span>${element.authors[0].verified? "<img class='inline' src='./icons/verified-icon.svg'>" :""}</span>
+                                    </p>
+                                    
+                                    <p class="font-medium text-slate-600">${element.others.views} views</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-        `
-        cardContainer.appendChild(div)
-    })
+            `
+            cardContainer.appendChild(div)
+        })
+    }
+    
 }
 
 handleCategory()
@@ -72,3 +82,4 @@ handleCard(1000)
 const blogButton =()=>{
     window.location.href = "blog.html"
 }
+
